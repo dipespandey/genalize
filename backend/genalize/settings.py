cff848b6d78aa16d672ba9da97309e86788bde85
dipespandey <pandey.dipesh50@gmail.com>
2025-07-19 19:24:40 +0200

    fix redirects

--- backend/genalize/settings.py
+++ backend/genalize/settings.py
@@ -19,16 +19,6 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(","
 
 APPEND_SLASH = True  # default is True but make it explicit
 
-
-if not DEBUG:
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
-
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -198,6 +188,9 @@ EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 
 # Security settings for production
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 if not DEBUG:
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
